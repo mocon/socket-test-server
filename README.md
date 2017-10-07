168210039c15459140d0bc80e74f8bc94cd4cc34
Myles O'Connor <hello@myles.la>
2017-10-07 14:24:15 -0700

    Setup server, added README

---
+++ README.md
@@ -0,0 +1,32 @@
+# Websocket test server
+
+A simple websocket server that sends the current formatted date once per second.
+
+## Requirements
+
+- [Node.js >= 8.6.0](https://nodejs.org/en/)
+- [Yarn package manager](https://yarnpkg.com/en/docs/install)
+
+## Install dependencies
+
+Use `yarn` to install the dependencies in `package.json`:
+
+```
+yarn
+```
+
+## Run server locally
+
+Run a local server with `nodemon`, restarting automatically when files are saved:
+
+```
+yarn start
+```
+
+## Clean install of dependencies
+
+Delete `node_modules`, clear the `yarn` cache, and re-install the dependencies:
+
+```
+yarn run clean
+```
---
+++ index.js
@@ -0,0 +1,40 @@
+// Clear console
+process.stdout.write('\x1B[2J\x1B[0f');
+
+const express = require('express');
+const http = require('http');
+const url = require('url');
+const WebSocket = require('ws');
+const moment = require('moment');
+const colour = require('colour');
+
+const app = express();
+
+app.use(function (req, res) {
+    res.send({ msg: 'hello' });
+});
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', function connection(ws, req) {
+    // You might use location.query.access_token to authenticate or share sessions
+    // or req.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
+    const location = url.parse(req.url, true);
+
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+    });
+
+    setInterval(() => {
+        const currentTime = Date.now();
+        const formattedTime = moment(currentTime).format('dddd, MMMM Do YYYY, h:mm:ss a');
+        ws.send(formattedTime);
+    }, 1000);
+
+    ws.send('Opened websocket connection.');
+});
+
+server.listen(8080, function listening() {
+    console.log('Listening on %d'.green, server.address().port);
+});
